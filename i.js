e23e6e0c96df4bbecb1c7fccbd48652761b07235
huytranwi1 <huytranwii12@gmail.com>
2023-05-04 20:17:22 -0700

    new lines added

---
+++ i.js
@@ -0,0 +1,55 @@
+const timmy = {
+  prescription: "acetaminophen",
+  pricePerRefill: 25,
+  refills: 3,
+  subscription: false,
+  coupon: true,
+};
+
+const sarah = {
+  prescription: "diphenhydramine",
+  pricePerRefill: 50,
+  refills: 1,
+  subscription: true,
+  coupon: false,
+};
+
+const rocky = {
+  prescription: "phenylephrine",
+  pricePerRefill: 30,
+  refills: 5,
+  subscription: true,
+  coupon: true,
+};
+
+// declaring variables for discounts
+let x = 0.25;
+let y = 10;
+
+// create a function that calculates the total price of prescription
+function totalPrice(pricePerRefill, refills) {
+  return pricePerRefill * refills;
+}
+
+// create a function that adds the subscription/coupon discount to the total amount using if/else statements
+function addDiscounts(customer) {
+  const totalAmount = totalPrice(customer.pricePerRefill, customer.refills);
+  if (customer.subscription && customer.coupon) {
+    return totalAmount - (totalAmount * x + y);
+  } else if (customer.subscription) {
+    return totalAmount - totalAmount * x;
+  } else if (customer.coupon) {
+    return totalAmount - y;
+  } else {
+    return totalAmount;
+  }
+}
+// printing the final amount of the prescription discounts for each customer
+const timmyFinalAmount = addDiscounts(timmy);
+console.log(`Timmy's Grand Total is $${timmyFinalAmount}.`);
+
+const sarahFinalAmount = addDiscounts(sarah);
+console.log(`Sarah's Grand Total is $${sarahFinalAmount}.`);
+
+const rockyFinalAmount = addDiscounts(rocky);
+console.log(`Rocky's Grand Total is $${rockyFinalAmount}.`);
--- index.js
+++ index.js
@@ -1,65 +1,73 @@
-
 const timmy = {
-    prescription: "acetaminophen",
-    pricePerRefill: 25,
-    refills: 3,
-    subscription: false,
-    coupon: true,
+  prescription: "acetaminophen",
+  pricePerRefill: 25,
+  refills: 3,
+  subscription: false,
+  coupon: true,
 };
 
 const sarah = {
-    prescription: "diphenhydramine",
-    pricePerRefill: 50,
-    refills: 1,
-    subscription: true,
-    coupon: false,
-}
+  prescription: "diphenhydramine",
+  pricePerRefill: 50,
+  refills: 1,
+  subscription: true,
+  coupon: false,
+};
 
 const rocky = {
-    prescription: "phenylephrine",
-    pricePerRefill: 30,
-    refills: 5,
-    subscription: true,
-    coupon: true,
-}
+  prescription: "phenylephrine",
+  pricePerRefill: 30,
+  refills: 5,
+  subscription: true,
+  coupon: true,
+};
 
 // create different function that take in an argument from the outside
 
 // we can change the timmy arguments and it will calculate the other people as well
 
-
 // we need the sum before any discount
-function customerName(price){
-    console.log(price.pricePerRefill + price.refills)
+function subTotal(price) {
+  return price.pricePerRefill * price.refills;
 }
-
-const name = customerName(timmy);
 
 // If a customer has a subscription, the customer will receive a 25% discount after the refill total has been calculated.
 
-function customerSubdis(discount){
-    if(discount.subscription == true) {
-        console.log((discount.pricePerRefill + discount.refills) - ((discount.pricePerRefill + discount.refills)*0.25));
-    } else {
-        console.log("no discount")
-    }
+function customerSubdis(person) {
+  const subTotal = subTotal(person);
+  let total = subTotal;
+  if (person.subscription == true) {
+    //const subTotal = discount.pricePerRefill + discount.refills;
+    console.log(subTotal - subTotal * 0.25);
+  }
+  if (couponDiscount.coupon === true) {
+    console.log(
+      couponDiscount.pricePerRefill +
+        couponDiscount.refills -
+        (couponDiscount.pricePerRefill + couponDiscount.refills) * 0.25 +
+        10
+    );
+  }
 }
 
 // subscription discount if the person has subscription or not
- const subDiscount = customerSubdis(timmy);
+const subDiscount = customerSubdis(timmy);
 
-
- // If the customer has a coupon, the customer will receive a $10 discount after the subscription discount has been calculated.
- function customerDiscount(couponDiscount) {
-    if(couponDiscount.coupon === true){
-        console.log((couponDiscount.pricePerRefill + couponDiscount.refills) - ((couponDiscount.pricePerRefill + couponDiscount.refills)*0.25) + 10)
-    } else {
-        console.log("no discount");
-    }
-
- }
- // argument for if coupon true or false
- const couDiscount = customerDiscount(timmy);
+// If the customer has a coupon, the customer will receive a $10 discount after the subscription discount has been calculated.
+function customerDiscount(couponDiscount) {
+  if (couponDiscount.coupon === true) {
+    console.log(
+      couponDiscount.pricePerRefill +
+        couponDiscount.refills -
+        (couponDiscount.pricePerRefill + couponDiscount.refills) * 0.25 +
+        10
+    );
+  } else {
+    console.log("no discount");
+  }
+}
+// argument for if coupon true or false
+const couDiscount = customerDiscount(timmy);
 
 // At the end of the script, you should return and log the string "Your grand total is ${finalAmount}."
- console.log("Your grand total is " + couDiscount);
+console.log("Your grand total is " + couDiscount);
